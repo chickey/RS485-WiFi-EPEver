6c7a4764cad22ece6ec10bdadc13518db8dc0887
Colin Hickey <chickey@eplop.co.uk>
2020-08-04 12:55:39 +0100

    Update mqtt.h
    Modification to make topic definable via interface.

--- mqtt.h
+++ mqtt.h
@@ -3,7 +3,7 @@ PubSubClient mqtt_client(wifi_client);
 
 const char* mqtt_username = "";
 const char* mqtt_password = "";
-const char* mqtt_topic = "";
+const char* mqtt_topic = "solar";
 
 char mqtt_msg[64];
 char buf[256];
@@ -60,12 +60,14 @@ void mqtt_reconnect() {
       Serial.println("connected");
       
       // Once connected, publish an announcement...
-      mqtt_client.publish("solar", "online");
+      mqtt_client.publish(mqtt_topic, "online");
       do_update = 1;
       
       // ... and resubscribe
-      mqtt_client.subscribe("solar/load/control");
-      mqtt_client.subscribe("solar/setting/sleep");
+      sprintf(buf,"%s/load/control",mqtt_topic);
+      mqtt_client.subscribe(buf);
+      sprintf(buf,"%s/setting/sleep",mqtt_topic);
+      mqtt_client.subscribe(buf);
       
       
     } else {
@@ -97,7 +99,8 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
 
     // solar/load/control
     //
-    if ( strncmp( topic, "solar/load/control", strlen("solar/load/control") ) == 0 ){
+    sprintf(buf,"%s/load/control",mqtt_topic);
+    if ( strncmp( topic, buf, strlen(buf) ) == 0 ){
 
         // Switch - but i can't seem to switch a coil directly here ?!?
         if ( strncmp( (char *) payload , "1",1) == 0 || strcmp( (char *) payload , "on") == 0  ) {
@@ -113,62 +116,75 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
     } 
 }
 
-char* concat(char* str1, char* str2){
-  char* result;
-  strcpy(result, str1);
-  strcat(result, str2);
-  return result;
-}
-
 void mqtt_publish() {
   // publish via mqtt
   //
   Serial.println("Publishing: ");
-    
   
   // time
   //
-  sprintf(buf, "20%02d-%02d-%02d %02d:%02d:%02d" , rtc.r.y , rtc.r.M , rtc.r.d , rtc.r.h , rtc.r.m , rtc.r.s);
-  mqtt_publish_s( "solar/status/time", buf );
+  //sprintf(buf, "20%02d-%02d-%02d %02d:%02d:%02d" , rtc.r.y , rtc.r.M , rtc.r.d , rtc.r.h , rtc.r.m , rtc.r.s);
+  //mqtt_publish_s( "solar/status/time", buf );
 
-  
   // panel
   // 
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic),"/panel/V"), live.l.pV /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/panel/I"), live.l.pI /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/panel/P"), live.l.pP /100.f);
-  
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/battery/V"), live.l.bV /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/battery/I"), live.l.bI /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/battery/P"), live.l.bP /100.f);
-  
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/load/V"), live.l.lV /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/load/I"), live.l.lI /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/load/P"), live.l.lP /100.f);
+  sprintf(buf,"%s/panel/V",mqtt_topic);
+  mqtt_publish_f( buf, live.l.pV /100.f);
+  sprintf(buf,"%s/panel/I",mqtt_topic);
+  mqtt_publish_f( buf, live.l.pI /100.f);
+  sprintf(buf,"%s/panel/P",mqtt_topic);
+  mqtt_publish_f( buf, live.l.pP /100.f);
 
+  sprintf(buf,"%s/battery/V",mqtt_topic);
+  mqtt_publish_f( buf, live.l.bV /100.f);
+  sprintf(buf,"%s/battery/I",mqtt_topic);
+  mqtt_publish_f( buf, live.l.bI /100.f);
+  sprintf(buf,"%s/battery/P",mqtt_topic);
+  mqtt_publish_f( buf, live.l.bP /100.f);
+  
+  sprintf(buf,"%s/load/V",mqtt_topic);  
+  mqtt_publish_f( buf, live.l.lV /100.f);
+  sprintf(buf,"%s/load/I",mqtt_topic);
+  mqtt_publish_f( buf, live.l.lI /100.f);
+  sprintf(buf,"%s/load/P",mqtt_topic);
+  mqtt_publish_f( buf, live.l.lP /100.f);
 
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/co2reduction/t"), stats.s.c02Reduction/100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/battery/SOC"),   batterySOC/1.0f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/battery/netI"),  batteryCurrent/100.0f);
-  mqtt_publish_s( concat(const_cast<char*>(mqtt_topic) , "/load/state"),    (char*) (loadState == 1? "on": "off") );  // pimatic state topic does not work with integers or floats ?!?
+  sprintf(buf,"%s/co2reduction/t",mqtt_topic);
+  mqtt_publish_f( buf,  stats.s.c02Reduction/100.f);
+  sprintf(buf,"%s/battery/SOC",mqtt_topic);
+  mqtt_publish_f( buf,    batterySOC/1.0f);
+  sprintf(buf,"%s/battery/netI",mqtt_topic);
+  mqtt_publish_f( buf,   batteryCurrent/100.0f);
+  sprintf(buf,"%s/load/state",mqtt_topic);
+  mqtt_publish_s( buf,    (char*) (loadState == 1? "on": "off") );  // pimatic state topic does not work with integers or floats ?!?
    
+  sprintf(buf,"%s/battery/minV",mqtt_topic);
+  mqtt_publish_f( buf,  stats.s.bVmin /100.f);
+  sprintf(buf,"%s/battery/maxV",mqtt_topic);
+  mqtt_publish_f( buf,  stats.s.bVmax /100.f);
   
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/battery/minV"), stats.s.bVmin /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/battery/maxV"), stats.s.bVmax /100.f);
-  
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/panel/minV"), stats.s.pVmin /100.f);
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/panel/maxV"), stats.s.pVmax /100.f);
-  
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/energy/consumed_day"), stats.s.consEnerDay/100.f );
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/energy/consumed_all"), stats.s.consEnerTotal/100.f );
+  sprintf(buf,"%s/panel/minV",mqtt_topic);
+  mqtt_publish_f( buf,  stats.s.pVmin /100.f);
+  sprintf(buf,"%s/panel/maxV",mqtt_topic);
+  mqtt_publish_f( buf,  stats.s.pVmax /100.f);
 
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/energy/generated_day"), stats.s.genEnerDay/100.f );
-  mqtt_publish_f( concat(const_cast<char*>(mqtt_topic) , "/energy/generated_all"),  stats.s.genEnerTotal/100.f );
+  sprintf(buf,"%s/energy/consumed_day",mqtt_topic);  
+  mqtt_publish_f( buf,  stats.s.consEnerDay/100.f );
+  sprintf(buf,"%s/energy/consumed_all",mqtt_topic);
+  mqtt_publish_f( buf,  stats.s.consEnerTotal/100.f );
 
+  sprintf(buf,"%s/energy/generated_day",mqtt_topic);
+  mqtt_publish_f( buf,  stats.s.genEnerDay/100.f );
+  sprintf(buf,"%s/energy/generated_all",mqtt_topic);
+  mqtt_publish_f( buf,   stats.s.genEnerTotal/100.f );
 
-  mqtt_publish_s( concat(const_cast<char*>(mqtt_topic) , "/status/batt_volt"), batt_volt_status[status_batt.volt] );
-  mqtt_publish_s( concat(const_cast<char*>(mqtt_topic) , "/status/batt_temp"), batt_temp_status[status_batt.temp] );
+  sprintf(buf,"%s/status/batt_volt",mqtt_topic);
+  mqtt_publish_s( buf, batt_volt_status[status_batt.volt] );
+  sprintf(buf,"%s/status/batt_temp",mqtt_topic);
+  mqtt_publish_s( buf, batt_temp_status[status_batt.temp] );
 
-  //mqtt_publish_s( "solar/status/charger_input", charger_input_status[ charger_input ]  );
-  mqtt_publish_s( strcat(const_cast<char*>(mqtt_topic) , "/status/charger_mode"),  charger_charging_status[ charger_mode ] );  
+  //sprintf(buf,"%s/status/charger_input",mqtt_topic);
+  //mqtt_publish_s( buf, charger_input_status[ charger_input ]  );
+  sprintf(buf,"%s/status/charger_mode",mqtt_topic);
+  mqtt_publish_s( buf,  charger_charging_status[ charger_mode ] );  
 }
